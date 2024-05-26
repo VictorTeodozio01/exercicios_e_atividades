fcd5da9829e0bcd61fceee003c925eb0e0a4af5d
victorTeodozio01 <victorteodozio@hotmail.com>
2024-05-26 18:17:22 -0300

    .NET DEVELOPE

---
+++ Construindo um Sistema para um Estacionamento com C#/Estacionamento/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Estacionamento estacionamento = new Estacionamento(10, 5);
+
+        bool continuar = true;
+        while (continuar)
+        {
+            Console.WriteLine("Digite a sua opção:");
+            Console.WriteLine("1 - Cadastrar veículo");
+            Console.WriteLine("2 - Remover veículo");
+            Console.WriteLine("3 - Listar veículos");
+            Console.WriteLine("4 - Encerrar");
+
+            string opcao = Console.ReadLine();
+
+            switch (opcao)
+            {
+                case "1":
+                    estacionamento.AdicionarVeiculo();
+                    break;
+
+                case "2":
+                    estacionamento.RemoverVeiculo();
+                    break;
+
+                case "3":
+                    estacionamento.ListarVeiculos();
+                    break;
+
+                case "4":
+                    continuar = false;
+                    break;
+
+                default:
+                    Console.WriteLine("Opção inválida!");
+                    break;
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
